b630776ef46e17996312fb5c151df60cd9e271f0
donavon <github@donavon.com>
2019-03-06 15:54:56 -0500

    Updates

---
+++ precache-manifest.275e56bec51c1d41dbd7340549610593.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7672b772d6bbf9965f5c",
+    "url": "/garage-door/static/js/runtime~main.7672b772.js"
+  },
+  {
+    "revision": "4d3590d30fb09a5b74f2",
+    "url": "/garage-door/static/js/main.4d3590d3.chunk.js"
+  },
+  {
+    "revision": "63fb9fa0fb68938d974c",
+    "url": "/garage-door/static/js/1.63fb9fa0.chunk.js"
+  },
+  {
+    "revision": "4d3590d30fb09a5b74f2",
+    "url": "/garage-door/static/css/main.8b2673d7.chunk.css"
+  },
+  {
+    "revision": "38b30c63be77336768d67def37bf13bc",
+    "url": "/garage-door/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fdbddc6d8deadb5f01280b47115a0cf4.js"
+  "/garage-door/precache-manifest.275e56bec51c1d41dbd7340549610593.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/garage-door/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
